05bb84437295c78216c9c0f8e7affdb4456e339d
Eva Maina <evajohnson714@gmail.com>
2018-12-19 11:34:50 +0300

    ft(social share):implement social sharing (#26)
    - enable user to share articles via facebook, mail,twitter and watsapp
     - test social share
     - fix hound issues
     - style buttons
    
    [Finishes #161254628]

--- src/components/ArticleView/ArticleViewComponents.js
+++ src/components/ArticleView/ArticleViewComponents.js
@@ -3,9 +3,10 @@ import 'bootstrap';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import propTypes from 'prop-types';
+import SocialShare from '../SocialShare/ShareComponent';
 
 export const Article = (props) => {
-  const { title, description, body } = props;
+  const { title, description, body, slug } = props;
   return (
     <div className="container-fluid">
       <h2>{title}</h2>
@@ -13,7 +14,14 @@ export const Article = (props) => {
         <p>{description}</p>
       </div>
       <div className="article-body">
-        {body}
+        <div className="row">
+          <div className="col-11">
+            <p>{body}</p>
+          </div>
+          <div className="col-1">
+            <SocialShare title={title} slug={slug} />
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -23,12 +31,14 @@ Article.propTypes = {
   title: propTypes.string,
   description: propTypes.string,
   body: propTypes.string,
+  slug: propTypes.string,
 };
 
 Article.defaultProps = {
   title: '',
   description: '',
   body: '',
+  slug: '',
 };
 
 
