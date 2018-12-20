a39138df228978256769490aad864e40f3bd1a1c
Abado Jack Mtulla <abadojack@gmail.com>
2018-12-20 10:02:23 +0300

    feat(logout): add logout functionality
    - add logout functionality
    - remove sign in/sign up links when a user is logged in
    - remove new articles link when new articles page is open
    
    [Delivers #162779394]

--- src/components/NavBar/_tests_/App.test.js
+++ src/components/NavBar/_tests_/App.test.js
@@ -1,9 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { NavBarComponent } from '../App';
+import { shallow } from 'enzyme';
+import { NavBarComponent, UnAuthed, AuthedNoNewArticle, AuthedNewArticle, FooterComponent } from '../App';
 
-it('footer render without crashing', () => {
-  const div = document.createElement('nav');
-  ReactDOM.render(<NavBarComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
+function setup() {
+  const props = {};
+
+  const enzymeWrapperNavBar = shallow(<NavBarComponent {...props} />);
+  const enzymeWrapperUnAuthed = shallow(<UnAuthed {...props} />);
+  const enzymeWrapperAuthedNoNewArticle = shallow(<AuthedNoNewArticle {...props} />);
+  const enzymeWrapperAuthedNewArticle = shallow(<AuthedNewArticle {...props} />);
+  const enzymeWrapperFooter = shallow(<FooterComponent {...props} />);
+
+  return {
+    props,
+    enzymeWrapperNavBar,
+    enzymeWrapperUnAuthed,
+    enzymeWrapperAuthedNoNewArticle,
+    enzymeWrapperAuthedNewArticle,
+    enzymeWrapperFooter,
+  };
+}
+
+describe('navbar component', () => {
+  localStorage.setItem('user', '{"email": "john@jake.jake", "username": "john", "token": "token"}');
+  it('render elements in navbar without crashing', () => {
+    const { enzymeWrapperNavBar } = setup();
+    expect(enzymeWrapperNavBar.find('div').length).toBe(4);
+  });
+});
+
+describe('unauthed component', () => {
+  it('render elements in unauthed without crashing', () => {
+    const { enzymeWrapperUnAuthed } = setup();
+    expect(enzymeWrapperUnAuthed.find('ul').length).toBe(1);
+    expect(enzymeWrapperUnAuthed.find('a').length).toBe(3);
+  });
+});
+
+describe('AuthedNoNewArticle component', () => {
+  it('render elements in AuthedNoNewArticle without crashing', () => {
+    const { enzymeWrapperAuthedNoNewArticle } = setup();
+    expect(enzymeWrapperAuthedNoNewArticle.find('ul').length).toBe(1);
+    expect(enzymeWrapperAuthedNoNewArticle.find('a').length).toBe(2);
+  });
+});
+
+describe('AuthedNewArticle component', () => {
+  it('render elements in AuthedNewArticle without crashing', () => {
+    const { enzymeWrapperAuthedNewArticle } = setup();
+    expect(enzymeWrapperAuthedNewArticle.find('ul').length).toBe(1);
+    expect(enzymeWrapperAuthedNewArticle.find('a').length).toBe(3);
+  });
+});
+
+describe('Footer component', () => {
+  it('render elements in FooterComponent without crashing', () => {
+    const { enzymeWrapperFooter } = setup();
+    expect(enzymeWrapperFooter.find('div').length).toBe(1);
+    expect(enzymeWrapperFooter.find('span').length).toBe(1);
+  });
 });
